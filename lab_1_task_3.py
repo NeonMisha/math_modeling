7c9f9ae6ac14b8f915f40045153f796c31de17ab
NeonMisha <73933067+NeonMisha@users.noreply.github.com>
2022-10-11 16:03:32 +0000

    gjudfyutf

---
+++ lab_1_task_3.py
@@ -0,0 +1,24 @@
+a = int(input ("x="))
+b = a ** 3
+c = b ** 0.5
+d = a+b/3
+e = c/d
+q = 4*a ** 7
+f = c/d+q-a ** 5+80
+g = a ** 4
+h = a ** 3
+v = a ** 2
+m = 27*g + 12*h + 5*v + 10
+l = m ** 0.5
+k = e*f*l
+print (k)
+
+
+
+print(3%2)
+print((3/2)%1)
+
+bb = (3%2) + (16.7*4.32)//1
+cc = 14.5 + 31%12 - (a**3.4)//1
+dd = bb/cc
+print (dd)
---
+++ lab_1_task_4.py
@@ -0,0 +1,10 @@
+a = [1,5,"good","bad"]
+
+b = [9,"blue","Red",11]
+u = a[1] + b[3]
+uu = a[2] + b[2]
+uuu = a[0] * b[0]
+uuuu = a[1] ** b[3]
+uuuuu = a+ b
+f = [u,uu,uuu,uuuu,uuuuu]
+print (f)
