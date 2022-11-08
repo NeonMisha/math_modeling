ff1aa31093ceefe34740d8fbdf893d1b66475a94
NeonMisha <73933067+NeonMisha@users.noreply.github.com>
2022-11-08 16:48:07 +0000

    Initial commit

---
+++ lab3_ex3.py
@@ -0,0 +1,5 @@
+import nampy as np
+
+t = np.arange(0,5,0.00001)
+x = x0 + vx0 * t
+y y0 + vy0 * t -g * t**2 / 2
---
+++ lab3_ex4.py
@@ -0,0 +1,6 @@
+for m in range(M):
+  print(f"{m}:")
+  time.sleep(0.5)
+  for n in range(N):
+    
+  
