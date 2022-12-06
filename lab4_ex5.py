00075f3b867e094e24177d859304b522a7c1cc9c
NeonMisha <73933067+NeonMisha@users.noreply.github.com>
2022-12-06 14:55:57 +0000

    jl

---
+++ lab4_ex5.py
@@ -0,0 +1,18 @@
+shir = 0 
+dl = 0 
+P = 3.14
+r = 0
+osn = 0 
+h = 0
+
+
+def pl_pr (shir, dl):
+    pl = shir * dl
+
+def pl_kr (p, r):
+    pl_k = p * r**2
+
+def pl_tr (osn, h):
+    p = 
+
+def area(*arg,** )
